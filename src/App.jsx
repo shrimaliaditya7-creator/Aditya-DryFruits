81c1c7bee097e2d7df64dca2ffee2ecfded35146
Aditya Shrimali <shrimali.aditya7@gmail.com>
2026-03-23 19:35:47 +0530

    final fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
-import { CartProvider, AuthProvider, UIProvider } from './context/AppContext'
+import { AppProvider } from './context/AppContext'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
-import CartDrawer from './components/CartDrawer'
-import AuthModal from './components/AuthModal'
-import CheckoutModal from './components/CheckoutModal'
-import Toast from './components/Toast'
-import FloatWA from './components/FloatWA'
 import Home from './pages/Home'
 
-export default function App() {
+// (add more pages here if you have them)
+
+function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <UIProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-          <Footer />
-          <CartDrawer />
-          <AuthModal />
-          <CheckoutModal />
-          <Toast />
-          <FloatWA />
-        </UIProvider>
-      </CartProvider>
-    </AuthProvider>
+    <AppProvider>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </AppProvider>
   )
 }
+
+export default App
